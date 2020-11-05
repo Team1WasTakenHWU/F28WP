820dc61454ad432ceaf176d9975e38c88f8565fd
JakeP2002 <71329946+JakeP2002@users.noreply.github.com>
2020-11-05 14:08:52 +0000

    zander stuff

--- F28WP-master/JS/main.js
+++ F28WP-master/JS/main.js
@@ -1,5 +1,32 @@
 
+var x ;
 function changeBGImage(){
-     document.body.background = "monke.jpg";
+     document.body.background = "../Assets/pixil-frame-0_3.png","../Assets/pixil-frame-0_4.png";
+    var x = document.getElementById("play-menu")
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    }
+    else{
+        x.style.display = "none";
+    }
+    }
+
+function showPlayer(){
+    var y = document.getElementById("game-canvas")
+    var playerImage = new Image(150, 150);
+    playerImage.src = "../Assets/guy.png";
+    if (x.style.display === "block") {
+    playerImage.display.display = "block";
+}
+    else {
+    playerImage.style.display ="none";
+}
+}
+
+function setTop() {
+    window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
+}
 
 
