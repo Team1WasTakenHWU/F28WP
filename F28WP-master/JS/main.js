56fcbde0fb64f1c583d52a6c1dca4fae42e24965
co38 <co38@hw.ac.uk>
2020-11-05 22:51:16 +0000

    Leaderboard on right

--- F28WP-master/JS/main.js
+++ F28WP-master/JS/main.js
@@ -1,4 +1,36 @@
+var x ;
 
 function changeBGImage(){
-     document.body.background = "../Assets/monke.jpg";
+    document.body.background = "../Assets/pixil-frame-0_3.png","../Assets/pixil-frame-0_4.png";
+    var x = document.getElementById("play-menu")
+    var l = document.getElementById("leaderboard")
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    }
+    else{
+      x.style.display = "none";
+      l.style.display = "block";
+    }
+    l.style.right = "0px";
+
+
+}
+
+
+function showPlayer(){
+    var y = document.getElementById("game-canvas")
+    var playerImage = new Image(150, 150);
+    playerImage.src = "../Assets/guy.png";
+    if (x.style.display === "block") {
+    playerImage.display.display = "block";
+}
+    else {
+    playerImage.style.display = "none";
+}
+}
+
+function setTop() {
+    window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+}
 }
